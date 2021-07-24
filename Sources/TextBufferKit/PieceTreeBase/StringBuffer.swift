205b4d94c814bc8db52d9d006f0f0bbbdaea3fe3
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2021-07-24 22:00:12 +0200

    StringBuffer Index

--- Sources/TextBufferKit/PieceTreeBase/StringBuffer.swift
+++ Sources/TextBufferKit/PieceTreeBase/StringBuffer.swift
@@ -26,7 +26,7 @@
 
 import Foundation
 
-public struct StringBuffer<V: RangeReplaceableCollection & RandomAccessCollection & Hashable> where V.Index == Int {
+public struct StringBuffer<V: RangeReplaceableCollection & RandomAccessCollection & Hashable> {
     var buffer: V
-    var lineStarts: [Int]
+    var lineStarts: [V.Index]
 }
